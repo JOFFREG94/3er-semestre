f171212860acd6c7604cb4ff4b18401b9f36a09b
JOFFRE G <jw.guamanq@uea.edu.ec>
2025-07-06 20:59:03 -0500

    semana7

---
+++ semana7/ParentesisBalanceados.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BalanceoParentesis
+{
+    static bool EstaBalanceado(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+        foreach (char c in expresion)
+        {
+            if (c == '(' || c == '[' || c == '{')
+                pila.Push(c);
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (pila.Count == 0) return false;
+                char tope = pila.Pop();
+                if ((c == ')' && tope != '(') ||
+                    (c == ']' && tope != '[') ||
+                    (c == '}' && tope != '{'))
+                    return false;
+            }
+        }
+        return pila.Count == 0;
+    }
+
+    static void Main()
+    {
+        string expresion = "{7 + (8 * 5) - [(9 - 7) + (4 + 1)]}";
+        if (EstaBalanceado(expresion))
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula desbalanceada.");
+    }
+}
