1f03b4da0c6f8f557bd50023dd7bbdc8d7e570e6
JOFFRE G <jw.guamanq@uea.edu.ec>
2025-07-06 20:57:33 -0500

    arreglolista

--- semana6/Program.cs
+++ semana6/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Lista
+{
+    private List<int> elementos;
+
+    public Lista()
+    {
+        elementos = new List<int>();
+    }
+
+    public void Agregar(int valor)
+    {
+        elementos.Add(valor);
+    }
+
+    public int Buscar(int dato)
+    {
+        int contador = 0;
+        foreach (int elemento in elementos)
+        {
+            if (elemento == dato)
+            {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public int ContarElementos()
+    {
+        return elementos.Count;
+    }
+
+    public void Mostrar()
+    {
+        Console.WriteLine("Elementos de la lista: " + string.Join(", ", elementos));
+    }
+}
+
+// Renombramos la clase para evitar conflicto con otra clase Program
+class MiPrograma
+{
+    static void Main(string[] args)
+    {
+        Lista miLista = new Lista();
+
+        miLista.Agregar(5);
+        miLista.Agregar(10);
+        miLista.Agregar(5);
+        miLista.Agregar(20);
+        miLista.Agregar(5);
+
+        miLista.Mostrar();
+
+        // Ejercicio 1: Contar elementos
+        int cantidad = miLista.ContarElementos();
+        Console.WriteLine("Número total de elementos: " + cantidad);
+
+        // Ejercicio 3: Buscar ocurrencias
+        int datoBuscar = 5;
+        int veces = miLista.Buscar(datoBuscar);
+        Console.WriteLine($"El número {datoBuscar} se encuentra {veces} veces en la lista.");
+    }
+}
